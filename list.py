f62f8cb62c1699e1995a26bf8b075956a960a7d8
sheldon <sheldon.xiej@gmail.com>
2018-05-10 15:28:01 +0800

    python变量基础类型

---
+++ list.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#list和tuple是Python内置的有序集合，一个可变，一个不可变。根据需要来选择使用它们。
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print(L[0][0]);
+# 打印Python:
+print(L[1][1]);
+# 打印Lisa:
+print(L[2][2]);
+
+
